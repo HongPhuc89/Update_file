ba696c6dd65fd6003bd87ea0deaaeb466569f056
Nguyễn Hồng Phúc <HongPhuc89@users.noreply.github.com>
2014-08-27 23:56:32 +0700

    Update copy_file.py
    copy necessary file

--- copy_file.py
+++ copy_file.py
@@ -8,10 +8,15 @@ import sys      #System lib
 #All function to update file anh dir
 ####################################################################################################
 #copy file
+fileCount = 0
 def copy_file(src_path,des_path):
-    if sys.platform == 'win32':
-        os.system('xcopy' % (src_path,des_path))
+    global fileCount
+    if sys.platform == 'linux':
+        pass
+        #from subprocess import call
+        #call(['xcopy', src_path, des_path, '/K/O/X'])
     else:
+        fileCount += 1
         shutil.copy2(src_path,des_path)
 
 #copy if old file
@@ -33,34 +38,51 @@ def new_dir(des_path):
     try:
         os.stat(des_path)
     except:
-        os.mkdir(des_path)
+        os.makedirs(des_path)
 
 #Get direction in des folder
 def get_des_dir(src_path,des_path,file_path):
     return file_path.replace(src_path,des_path)
+#get the extra name of file
+def get_extra_name(file):
+    return(file[file.find('.')+1:len(file)])
+#check C file
+def check_c_file(file):
+    cFileExt = ['c','h','yml']
+    readme  = 'readme.txt'
+    if file.find(readme) > -1:
+        return 1
+    if get_extra_name(file) in cFileExt:
+        return 1
+    else:
+        return 0
+
 #search all find in folder
 ####################################################################################################
 def update_folder(src_path,des_path):
     for root, dirs, files in os.walk(src_path):
         #copy folder
-        for dir in dirs:
-            src_dir     = root+'/'+dir
-            des_dir     = get_des_dir(src_path,des_path,src_dir)
-            new_dir(des_dir)
+        #for dir in dirs:
+        #    src_dir     = root+'/'+dir
+        #    des_dir     = get_des_dir(src_path,des_path,src_dir)
+        #    new_dir(des_dir)
         #copy file
         for file in files:
-            src_file    = root+'/'+file
-            des_file    = get_des_dir(src_path,des_path,src_file)
-            update_file(src_file,des_file)
+            if check_c_file(file) == 1:
+                des_root    = get_des_dir(src_path,des_path,root)
+                new_dir(des_root)
+                src_file    = root+'/'+file
+                des_file    = get_des_dir(src_path,des_path,src_file)
+                update_file(src_file,des_file)
 
 #main programme
 start=time.time()
 
-src_path        = ur"D:/WorkSpace/"
-des_path        = ur"H:/"
+src_path        = ur"D:/C/sdk"
+des_path        = ur"C:/Documents and Settings/Hong Phuc/Desktop/output/"
 print "Copying file..."
 update_folder(src_path,des_path)
 finish          = time.time()
+print "Copy",fileCount,"files to new folder"
 print "Finished in:", int(finish-start),'s'
-print finish-start
-time.sleep(2)
+ch = raw_input('Press enter to exit')
